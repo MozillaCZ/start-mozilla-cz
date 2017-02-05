a3666728d1c0b974806683128a8b806b20136dee
Tomáš Zelina <zelitomas@gmail.com>
2017-02-05 19:19:37 +0100

    "Opravit skryté boxy" zkráceno na "Opravit skryté"
    Skryté je tak nyní zpodstanělé přídavné jméno.

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 		Copyright &copy; 2009&ndash;<?php echo date('Y'); ?> <a href="https://www.mozilla.cz/">Mozilla.cz</a>,&nbsp;zpravodajství 
                 dodávají <a href="http://www.ceskenoviny.cz/">ČeskéNoviny.cz</a>,
                 hostováno <a href="http://www.cesky-hosting.cz/">Český hosting</a>. 
-                <?php if(!isset($_COOKIE['hide-box']) || $_COOKIE['hide-box'] != "") echo'<a href="?hide-box=">Obnovit skryté boxy</a>'; ?>
+                <?php if(!isset($_COOKIE['hide-box']) || $_COOKIE['hide-box'] != "") echo'<a href="?hide-box=">Obnovit skryté</a>'; ?>
 	</p>
 </div>
 <script type="text/javascript" src="/js/google-analytics.js"></script>
