a2dc3f045af2a03aef3e32155dd3b76eea33736e
Michal Stanke <michal@stanke.cz>
2018-07-07 17:28:37 +0200

    Cron pro pravidelné sestavení pomocí Travis CI
    https://docs.travis-ci.com/user/triggering-builds/

--- _cron/start-mozilla-cz-cron.php
+++ _cron/start-mozilla-cz-cron.php
@@ -1,11 +1,34 @@
 <?php
-#$url = 'https://api.netlify.com/build_hooks/...';
+$url = 'https://api.travis-ci.org/repo/MozillaCZ%2Fstart-mozilla-cz/requests';
+$body = '{ "request": { "branch":"master" } }';
+// $token = 'xxx';
 $opts = array(
     'http' => array(
         'method'  => 'POST',
-        'header' => "Content-Type: application/x-www-form-urlencoded\r\nContent-Length: 0",
+        'header' =>
+          "Content-Type: application/json\r\n" .
+          "Content-Length: " . strlen($body) . "\r\n" .
+          "Accept: application/json\r\n" .
+          "Travis-API-Version: 3\r\n" .
+          "Authorization: token " . $token . "\r\n",
+        'content' => $body,
+    ),
+);
+$context = stream_context_create($opts);
+file_get_contents($url, false, $context);
+
+/*
+<?php
+// $url = 'https://api.netlify.com/build_hooks/...';
+$opts = array(
+    'http' => array(
+        'method'  => 'POST',
+        'header' =>
+          "Content-Type: application/x-www-form-urlencoded\r\n" .
+          "Content-Length: 0",
         'content' => ''
     ),
 );
 $context = stream_context_create($opts);
-$result = file_get_contents($url, false, $context);
+file_get_contents($url, false, $context);
+*/
