400e22072a3de417a7812145d524181c6bbb271e
Michal Stanke <michal@stanke.cz>
2019-11-09 11:58:44 +0100

    Zjednodušení pluginu pro stahování dat

--- _plugins/Jekyll_Get_Remote_Data.rb
+++ _plugins/Jekyll_Get_Remote_Data.rb
@@ -15,30 +15,20 @@ module Jekyll_Get_Remote_Data
       if !config.kind_of?(Array)
         config = [config]
       end
-      site.data['rss_feeds'] = [];
+      site.data['rss_feeds'] = Hash.new
       config.each do |d|
-        case d['type'].downcase
-          when 'json'
-            json(site, d)
-          when 'rss'
-            rss(site, d)
-          else
-            raise "Unknown type '#{d['type']}' of '#{d['name']}'. Supported are 'json' and 'rss'."
-          end
-      end
-    end
-
-    def json(site, d)
-      open(d['url'], 'r') do |file|
-        site.data[d['name']] = JSON.parse(file.read)
-        site.data[d['name']]['origin_url'] = d['url']
-      end
-    end
-
-    def rss(site, d)
-      open(d['url'], 'r') do |file|
-        site.data['rss_feeds'].push(d['name'])
-        site.data[d['name']] = JSON.parse(FeedParser::Parser.parse(file.read).to_json)
+        file_as_json = nil
+        open(d['url'], 'r') do |file|
+          case d['type'].downcase
+            when 'json'
+              file_as_json = file.read
+            when 'rss'
+              file_as_json = FeedParser::Parser.parse(file.read).to_json
+            else
+              raise "Unknown type '#{d['type']}' of '#{d['name']}'. Supported are 'json' and 'rss'."
+            end
+        end
+        site.data[d['name']] = JSON.parse(file_as_json)
         site.data[d['name']]['origin_url'] = d['url']
       end
     end
